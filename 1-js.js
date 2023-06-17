932853bea2dad47bfd776342ccfb524e86059b18
Júlyin 8) <86024663+XMegaLatiosX@users.noreply.github.com>
2023-06-17 12:31:48 -0300

    Update 1-js.js
    now buttons have animation when disabled and h1 glows up with text shadow

--- 1-js.js
+++ 1-js.js
@@ -1,8 +1,14 @@
-let options = ['picles', 'dinhero', 'assasinato', 'minecraft', 'penis', 'pq?', 'OF', 'lol', '5$', 'cpf', 'vaza cartao']
+// ['picles', 'dinhero', 'assasinato', 'minecraft', 'penis', 'pq?', 'OF', 'lol', '5$', 'cpf', 'vaza cartao']
+let options = ['A', 'N', 'A', 'A', 'P', 'K', '💣', '1$', '3$', '5$', '5k points', 'SUB']
 
 function show(btn) {
     btn.disabled = true
-    btn.innerHTML = shuffle();
+    btn.style = "animation: spin-faster 5s cubic-bezier(0, 0, 0.66, 0.83) 1; ";
+    setTimeout(() => {
+        btn.firstChild.textContent = shuffle();
+        btn.firstChild.style = "animation: glow 3s linear infinite;";
+        btn.style = "animation: spin-slower 5s cubic-bezier(0, 0, 0.66, 0.83) 1; text-shadow: 0px -1px 3px red, 0 3px red, -1px 0 3px red, 1px 0 3px red;";
+    }, 5000);
 }
 
 function shuffle() {
